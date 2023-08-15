04e9a222466f2038cd9d5fd358da9e543e6ee4b5
shannoj <69207315+shannoj@users.noreply.github.com>
2023-08-15 12:15:25 +0200

    added function to add inner html to main so that the book that is entered is displayed on the site

--- javascript.js
+++ javascript.js
@@ -17,6 +17,10 @@ let formContainer = document.getElementById('form-container');
 
 let submit = document.getElementById("submit");
 
+let form = document.getElementById("form");
+
+let main = document.getElementById("main");
+
 addButton.addEventListener("click", function(){
     formContainer.style.display = "flex";
 })
@@ -25,4 +29,37 @@ window.addEventListener("click", function(event) {
     if (event.target === formContainer) {
         formContainer.style.display = "none";
     }
+});
+
+form.addEventListener("submit", function(e){
+    e.preventDefault();
+
+    const title = document.getElementById("title").value;
+    const author = document.getElementById("author").value;
+    const pages = document.getElementById("pages").value;
+    var read = '';
+
+    if (document.getElementById('yes').checked){
+        read = 'yes';
+    }
+
+    else {
+        read = 'no';
+    }
+
+    const book = new Book(title, author, pages, read);
+    addBook(book);
+
+    main.innerHTML += `
+    <div class="book"> 
+        <div class="book-item">${title}</div>
+        <div class="book-item">${author}</div>
+        <div class="book-item">${pages} pages</div>
+        <div class="book-item"> Have read? ${read}</div>
+    </div>
+    ` ;
+
+    form.reset();
+    formContainer.style.display = "none";
+
 });
